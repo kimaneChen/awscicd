14c58853ac7d2ffd34693741969ef34ffb4f2ca6
Kimane <chenjx1024@outlook.com>
2022-12-29 16:38:11 +1000

    modify the version

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3001;
 
-const version = '1112.0.1';
+const version = '3333.0.1';
 
 app.get('/', (req, res) => {
 	// set response content    
 	res.send(`<html>
                     <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: Amazing  Like & Subscribe!</h1>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]:Issue & Subscribe!</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
